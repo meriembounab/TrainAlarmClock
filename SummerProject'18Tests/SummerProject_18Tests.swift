e8c0dcd896095ba2c3594134db0c9e3300d9f999
Meriem <mbounab@purdue.edu>
2018-05-28 01:00:02 -0500

    Initial Commit

---
+++ SummerProject'18Tests/SummerProject_18Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SummerProject_18Tests.swift
+//  SummerProject'18Tests
+//
+//  Created by Meriem Bounab on 5/28/18.
+//  Copyright © 2018 Meriem Bounab. All rights reserved.
+//
+
+import XCTest
+@testable import SummerProject_18
+
+class SummerProject_18Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
