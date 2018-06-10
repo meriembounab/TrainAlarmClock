ba5fb6641ac3818acb23bb7761e8db8244e63288
Meriem <mbounab@purdue.edu>
2018-06-10 14:55:24 -0500

    added choosing line screen and choose destination screen

--- SummerProject'18/ChooseDestination.swift
+++ SummerProject'18/ChooseDestination.swift
@@ -1,20 +1,23 @@
-//  ViewController.swift
+//
+//  ChooseDestination.swift
 //  SummerProject'18
 //
-//  Created by Meriem Bounab on 5/28/18.
+//  Created by Meriem Bounab on 6/10/18.
 //  Copyright © 2018 Meriem Bounab. All rights reserved.
 //
+
 import UIKit
 import Lottie
 
-class ChooseLine: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource
+class ChooseDestination:UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
 {
-    @IBOutlet weak var pickerview: UIPickerView!
+    
+    @IBOutlet weak var pickerDest: UIPickerView!
     @IBOutlet weak var pickerlabel: UILabel!
-    let trainLines = ["Union Pacific North" , "Milwaukee North" , "North Central Service" , "Union Pacific Northwest" , "Milwaukee West" , "Union Pacific West" , "BNSF Railway" , "Heritage Corridor" , "SouthWest Service" , "Rock Island District" , "Metra Electric District"]
-    let animationView = LOTAnimationView(name:"border1")
-    override func viewDidLoad()
-    {
+    
+     let trainLines = ["Union Pacific North" , "Milwaukee North" , "North Central Service" , "Union Pacific Northwest" , "Milwaukee West" , "Union Pacific West" , "BNSF Railway" , "Heritage Corridor" , "SouthWest Service" , "Rock Island District" , "Metra Electric District"]
+    let animationView = LOTAnimationView(name: "border1")
+    override func viewDidLoad() {
         super.viewDidLoad()
         animationView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width , height: 42)
         animationView.contentMode = .scaleAspectFill
@@ -25,9 +28,8 @@ class ChooseLine: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource
         
         animationView.play()
         
-        pickerview.dataSource = self
-        pickerview.delegate = self
-        
+        pickerDest.dataSource = self
+        pickerDest.delegate = self
     }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -42,14 +44,10 @@ class ChooseLine: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         pickerlabel.text = trainLines[row]
     }
-   
+    
     
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
     }
-    
-    
-    
 }
-
